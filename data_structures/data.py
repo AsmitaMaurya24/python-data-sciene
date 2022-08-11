31254538e0fa18ad26586a861406218c2994fc60
AsmitaMaurya24 <asmitamaurya24@gmail.com>
2022-08-11 18:40:49 +0530

    Update data.py

--- data_structures/data.py
+++ data_structures/data.py
@@ -1,1 +1,6 @@
-story = '''Short on cash, undergrad David Blake (Dirk Benedict) takes a job working as a lab assistant for snake expert Dr. Carl Stoner (Strother Martin). When Stoner begins giving David a series of antibiotics as a safeguard against snakebites, he at first thinks nothing of it. But gradually David begins to notices curious changes in his body, and his new girlfriend, Dr. Stoner's daughter, Kristina (Heather Menzies), notices them too. Could Dr. Stoner really be trying to turn his assistant into a snake?'''
+story = '''Short on cash, undergrad David Blake (Dirk Benedict) takes a job working as a lab assistant for snake expert
+           Dr. Carl Stoner (Strother Martin).
+           When Stoner begins giving David a series of antibiotics as a safeguard against snakebites,
+           he at first thinks nothing of it. But gradually David begins to notices curious changes in his body,
+           and his new girlfriend, Dr. Stoner's daughter, Kristina (Heather Menzies), notices them too.
+           Could Dr. Stoner really be trying to turn his assistant into a snake?'''

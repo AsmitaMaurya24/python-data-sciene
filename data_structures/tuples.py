cd3422a4129b7b3fb3b9d7e1c69bc862b59e1169
AsmitaMaurya24 <asmitamaurya24@gmail.com>
2022-09-13 11:34:17 +0530

    tuple

--- data_structures/tuples.py
+++ data_structures/tuples.py
@@ -22,6 +22,7 @@ n_tuple = ('mouse', [8, 4, 6], (1, 2, 3))
 # nested index
 print(n_tuple[0][3])     # 's'
 print(n_tuple[1][1])     #  4
+print(n_tuple[2][0])     #  1
 
 
 # Accessing tuple elements using slicing

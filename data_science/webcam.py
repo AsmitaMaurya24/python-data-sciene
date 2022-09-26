780d742cffb8baf511b8b1d1f680441610f2ee1b
AsmitaMaurya24 <asmitamaurya24@gmail.com>
2022-09-26 15:40:08 +0530

    open_cv

--- data_science/webcam.py
+++ data_science/webcam.py
@@ -6,7 +6,7 @@ while True:
     status, frame = video.read()
     if not status:
         break
-    cv2.imshow("webcam 0",frame)    #displays a window with the webcam
+    cv2.imshow("webcam 0",frame)    # displays a window with the webcam
     if cv2.waitKey(1) == ord('q'):  # if the key is q, the loop breaks
         break
 video.release()                     # releases the webcam
